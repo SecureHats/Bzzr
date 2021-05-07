9f9e001bb78f87ec26d6d618923693938f2746e7
SecureHats <72928684+SecureHats@users.noreply.github.com>
2021-05-07 13:09:14 +0200

    Create PowerShell.DnsAlert.ps1

---
+++ PowerShell.DnsAlert.ps1
@@ -0,0 +1,23 @@
+Resolve-DnsName bbcnewsv2vjtpsuy.onion.to
+Resolve-DnsName all.mainnet.ethdisco.net
+Resolve-DnsName micros0ft.com 
+Resolve-DnsName 164e9408d12a701d91d206c6ab192994.info 
+
+For($i=0; $i -le 150; $i++) {
+$rand = -join ((97..122) | Get-Random -Count 32 | % {[char]$_})
+Resolve-DnsName "$rand.com"
+}
+
+$rand = -join ((97..122) | Get-Random -Count 63 | % {[char]$_})
+Resolve-DnsName "$rand.contoso.com"
+
+For($i=0; $i -le 1000; $i++) {
+
+$rand = -join ((97..122) | Get-Random -Count 63 | % {[char]$_})
+Resolve-DnsName "$rand.contoso.com"
+}
+
+Resolve-DnsName reseed.i2p-projekt.de 
+
+Write-Host -NoNewLine 'Press any key to continue...';
+$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
